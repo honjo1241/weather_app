2ee4c9b3051f0d7f0f35457582e3c9900d46e4f7
山田恭輔 <yamadakyousuke@yamadakyousuke-no-macbook-air.local>
2018-03-03 13:47:21 +0900

    Initial Commit

---
+++ weather_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_app
+//
+//  Created by 山田恭輔 on 2018/03/03.
+//  Copyright © 2018年 山田恭輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
